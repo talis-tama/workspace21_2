29e5cec2a1f4f4ae9e29426ec1d82ca297413d4b
江原 寛貴 <tama8445@gmail.com>
2018-04-01 17:22:13 +0900

    プロジェクト ファイルを追加します。

---
+++ classinfo/Data.cs
@@ -0,0 +1,327 @@
+﻿using Android.App;
+using System;
+
+namespace classinfo
+{
+    public partial class MainActivity
+    {
+        void ShowData_mon()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "空き";
+            textView19.Text = "----";
+            textView21.Text = "空き";
+            textView22.Text = "----";
+            textView24.Text = "空き";
+            textView25.Text = "----";
+            textView27.Text = "空き";
+            textView28.Text = "----";
+        }
+        void ShowData_tue()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "空き";
+            textView19.Text = "----";
+            textView21.Text = "空き";
+            textView22.Text = "----";
+            textView24.Text = "ウェブデザイン論";
+            textView25.Text = "3103";
+            textView27.Text = "空き";
+            textView28.Text = "----";
+        }
+        void ShowData_wed()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "システムプログラミング演習";
+            textView19.Text = "7501";
+            textView21.Text = "英語IIIA";
+            textView22.Text = "7308";
+            textView24.Text = "空き";
+            textView25.Text = "----";
+            textView27.Text = "現代自然科学";
+            textView28.Text = "3404";
+        }
+        void ShowData_thr()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "記録管理論";
+            textView19.Text = "7301";
+            textView21.Text = "空き";
+            textView22.Text = "----";
+            textView24.Text = "空き";
+            textView25.Text = "----";
+            textView27.Text = "空き";
+            textView28.Text = "----";
+        }
+        void ShowData_fri()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "プレゼミナールIII";
+            textView19.Text = "3104";
+            textView21.Text = "情報処理概論";
+            textView22.Text = "7203";
+            textView24.Text = "空き";
+            textView25.Text = "----";
+            textView27.Text = "空き";
+            textView28.Text = "----";
+        }
+        void ShowData_sat()
+        {
+            textView15.Text = "空き";
+            textView16.Text = "----";
+            textView18.Text = "空き";
+            textView19.Text = "----";
+            textView21.Text = "森林文化I";
+            textView22.Text = "3103";
+            textView24.Text = "森林文化I";
+            textView25.Text = "3103";
+            textView27.Text = "空き";
+            textView28.Text = "----";
+        }
+        void PopData_mon()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    break;
+            }
+        }
+        void PopData_tue()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                case 4:
+                    dialog.SetTitle("火曜日4限");
+                    dialog.SetMessage("授業名 ウェブデザイン論" + Environment.NewLine + "時間 15:00～16:30" + Environment.NewLine + "講師名 本池 巧" + Environment.NewLine + "教室 3103");
+                    dialog.Create().Show();
+                    break;
+                case 5:
+                    break;
+            }
+        }
+        void PopData_wed()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                    break;
+                case 2:
+                    dialog.SetTitle("水曜日2限");
+                    dialog.SetMessage("システムプログラミング演習" + Environment.NewLine + "時間 11:00～12:30" + Environment.NewLine + "講師名 水沼 友宏" + Environment.NewLine + "教室 7501");
+                    dialog.Create().Show();
+                    break;
+                case 3:
+                    dialog.SetTitle("水曜日3限");
+                    dialog.SetMessage("授業名 英語IIIA" + Environment.NewLine + "時間 13:20～14:50" + Environment.NewLine + "講師名 石川 真知子" + Environment.NewLine + "教室 7308");
+                    dialog.Create().Show();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    dialog.SetTitle("水曜日5限");
+                    dialog.SetMessage("現代自然科学I" + Environment.NewLine + "時間 16:40～18:10" + Environment.NewLine + "講師名 本池 巧" + Environment.NewLine + "教室 3404");
+                    dialog.Create().Show();
+                    break;
+            }
+        }
+        void PopData_thr()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                    break;
+                case 2:
+                    dialog.SetTitle("木曜日2限");
+                    dialog.SetMessage("授業名 記録管理論" + Environment.NewLine + "時間 11:00～12:30" + Environment.NewLine + "講師名 吉国 三千子" + Environment.NewLine + "教室 7301");
+                    dialog.Create().Show();
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    break;
+            }
+        }
+        void PopData_fri()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                    break;
+                case 2:
+                    dialog.SetTitle("金曜日2限");
+                    dialog.SetMessage("授業名 プレゼミナールIII" + Environment.NewLine + "時間 11:00～12:30" + Environment.NewLine + "講師名 本池 巧" + Environment.NewLine + "教室 3104");
+                    dialog.Create().Show();
+                    break;
+                case 3:
+                    dialog.SetTitle("金曜日3限");
+                    dialog.SetMessage("授業名 情報処理概論" + Environment.NewLine + "時間 13:20～14:50" + Environment.NewLine + "講師名 寺嶋 秀美" + Environment.NewLine + "教室 7203");
+                    dialog.Create().Show();
+                    break;
+                case 4:
+                case 5:
+                    break;
+            }
+        }
+        void PopData_sat()
+        {
+            var dialog = new AlertDialog.Builder(this);
+            switch (popNo)
+            {
+                case 1:
+                case 2:
+                    break;
+                case 3:
+                    dialog.SetTitle("土曜日3限");
+                    dialog.SetMessage("授業名 森林文化I" + Environment.NewLine + "時間 13:20～14:50" + Environment.NewLine + "講師名 原聰 他" + Environment.NewLine + "教室 3103");
+                    dialog.Create().Show();
+                    break;
+                case 4:
+                    dialog.SetTitle("土曜日4限");
+                    dialog.SetMessage("授業名 森林文化I" + Environment.NewLine + "時間 13:20～14:50" + Environment.NewLine + "講師名 原聰 他" + Environment.NewLine + "教室 3103");
+                    dialog.Create().Show();
+                    break;
+                case 5:
+                    break;
+            }
+        }
+        void ClassData_mon()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    ClassData_end();
+                    break;
+            }
+        }
+        void ClassData_tue()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    textView6.Text = "ウェブデザイン論";
+                    textView7.Text = "15:00～16:30";
+                    textView10.Text = "本池 巧";
+                    textView11.Text = "3103";
+                    break;
+                case 5:
+                    ClassData_end();
+                    break;
+            }
+        }
+        void ClassData_wed()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                    textView6.Text = "システムプログラミング演習";
+                    textView7.Text = "11:00～12:30";
+                    textView10.Text = "水沼 友宏";
+                    textView11.Text = "7501";
+                    break;
+                case 3:
+                    textView6.Text = "英語IIIA";
+                    textView7.Text = "13:20～14:50";
+                    textView10.Text = "石川 真知子";
+                    textView11.Text = "7308";
+                    break;
+                case 4:
+                case 5:
+                    textView6.Text = "現代自然科学I";
+                    textView7.Text = "16:40～18:10";
+                    textView10.Text = "本池 巧";
+                    textView11.Text = "3404";
+                    break;
+            }
+        }
+        void ClassData_thr()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                    textView6.Text = "記録管理論";
+                    textView7.Text = "11:00～12:30";
+                    textView10.Text = "吉国 三千子";
+                    textView11.Text = "7301";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    ClassData_end();
+                    break;
+            }
+        }
+        void ClassData_fri()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                    textView6.Text = "プレゼミナールIII";
+                    textView7.Text = "11:00～12:30";
+                    textView10.Text = "本池 巧";
+                    textView11.Text = "3104";
+                    break;
+                case 3:
+                    textView6.Text = "情報処理概論";
+                    textView7.Text = "13:20～14:50";
+                    textView10.Text = "寺嶋 秀美";
+                    textView11.Text = "7203";
+                    break;
+                case 4:
+                case 5:
+                    ClassData_end();
+                    break;
+            }
+        }
+        void ClassData_sat()
+        {
+            switch (classNo)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    textView6.Text = "森林文化I";
+                    textView7.Text = "13:20～14:50";
+                    textView10.Text = "原聰 他";
+                    textView11.Text = "3103";
+                    break;
+                case 4:
+                    textView6.Text = "森林文化I";
+                    textView7.Text = "15:00～16:30";
+                    textView10.Text = "原聰 他";
+                    textView11.Text = "3103";
+                    break;
+                case 5:
+                    ClassData_end();
+                    break;
+            }
+        }
+    }
+}
---
+++ classinfo/MainActivity.cs
@@ -0,0 +1,252 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Text;
+
+namespace classinfo
+{
+    [Activity(Label = "時間割", MainLauncher = true)]
+    public partial class MainActivity : Activity
+    {
+        TextClock textClock2;
+        TextView textView2, textView3, textView4, textView5, textView6, textView7,textView8, textView9, textView10, textView11, textView12, textView14, textView15, textView16, textView17, textView18, textView19, textView20, textView21, textView22, textView23, textView24, textView25, textView26, textView27, textView28;
+        Button button1, button2;
+        DateTime tim;
+        int weekNo = 0, showNo = 0, popNo = 0, classNo = 0;
+        bool first = true;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            tim = new DateTime();
+            Initilizing();
+        }
+        void Initilizing()
+        {
+            textClock2 = (TextClock)FindViewById(Resource.Id.textClock2);
+            textView2 = (TextView)FindViewById(Resource.Id.textView2);
+            textView3 = (TextView)FindViewById(Resource.Id.textView3);
+            textView4 = (TextView)FindViewById(Resource.Id.textView4);
+            textView5 = (TextView)FindViewById(Resource.Id.textView5);
+            textView6 = (TextView)FindViewById(Resource.Id.textView6);
+            textView7 = (TextView)FindViewById(Resource.Id.textView7);
+            textView8 = (TextView)FindViewById(Resource.Id.textView8);
+            textView9 = (TextView)FindViewById(Resource.Id.textView9);
+            textView10 = (TextView)FindViewById(Resource.Id.textView10);
+            textView11 = (TextView)FindViewById(Resource.Id.textView11);
+            textView12 = (TextView)FindViewById(Resource.Id.textView12);
+            textView14 = (TextView)FindViewById(Resource.Id.textView14);
+            textView15 = (TextView)FindViewById(Resource.Id.textView15);
+            textView16 = (TextView)FindViewById(Resource.Id.textView16);
+            textView17 = (TextView)FindViewById(Resource.Id.textView17);
+            textView18 = (TextView)FindViewById(Resource.Id.textView18);
+            textView19 = (TextView)FindViewById(Resource.Id.textView19);
+            textView20 = (TextView)FindViewById(Resource.Id.textView20);
+            textView21 = (TextView)FindViewById(Resource.Id.textView21);
+            textView22 = (TextView)FindViewById(Resource.Id.textView22);
+            textView23 = (TextView)FindViewById(Resource.Id.textView23);
+            textView24 = (TextView)FindViewById(Resource.Id.textView24);
+            textView25 = (TextView)FindViewById(Resource.Id.textView25);
+            textView26 = (TextView)FindViewById(Resource.Id.textView26);
+            textView27 = (TextView)FindViewById(Resource.Id.textView27);
+            textView28 = (TextView)FindViewById(Resource.Id.textView28);
+            button1 = (Button)FindViewById(Resource.Id.button1);
+            button2 = (Button)FindViewById(Resource.Id.button2);
+            WeekReader();
+            TimeReader();
+            textClock2.TextChanged += TextClock2_textChanged;
+            button1.Click += Button1_click;
+            button2.Click += Button2_click;
+            textView14.Click += First_click;
+            textView15.Click += First_click;
+            textView16.Click += First_click;
+            textView17.Click += Second_click;
+            textView18.Click += Second_click;
+            textView19.Click += Second_click;
+            textView20.Click += Third_click;
+            textView21.Click += Third_click;
+            textView22.Click += Third_click;
+            textView23.Click += Fourth_click;
+            textView24.Click += Fourth_click;
+            textView25.Click += Fourth_click;
+            textView26.Click += Fifth_click;
+            textView27.Click += Fifth_click;
+            textView28.Click += Fifth_click;
+        }
+        void TextClock2_textChanged(object sender,TextChangedEventArgs e)
+        {
+            if (textClock2.Text == ")00:00") { WeekReader(); }
+            else if (textClock2.Text == ")09:20") { classNo = 2; }
+            else if (textClock2.Text == ")11:00") { classNo = 3; }
+            else if (textClock2.Text == ")13:20") { classNo = 4; }
+            else if (textClock2.Text == ")15:00") { classNo = 5; }
+            else if (textClock2.Text == ")16:40") { ClassData_end(); }
+            if (textClock2.Text == ")09:20" || textClock2.Text == ")11:00" || textClock2.Text == ")13:20" || textClock2.Text == ")15:00") { ClassData_jumper(); }
+        }
+        void Button1_click(object sender,EventArgs e)
+        {
+            showNo--;
+            if (showNo == 0) { showNo = 6; }
+            ShowData_jumper();
+        }
+        void Button2_click(object sender,EventArgs e)
+        {
+            showNo++;
+            if (showNo == 7) { showNo = 1; }
+            ShowData_jumper();
+        }
+        void First_click(object sender, EventArgs e)
+        {
+            popNo = 1;
+            PopData_jumper();
+        }
+        void Second_click(object sender, EventArgs e)
+        {
+            popNo = 2;
+            PopData_jumper();
+        }
+        void Third_click(object sender, EventArgs e)
+        {
+            popNo = 3;
+            PopData_jumper();
+        }
+        void Fourth_click(object sender, EventArgs e)
+        {
+            popNo = 4;
+            PopData_jumper();
+        }
+        void Fifth_click(object sender, EventArgs e)
+        {
+            popNo = 5;
+            PopData_jumper();
+        }
+        void WeekReader()
+        {
+            tim = DateTime.Now;
+            textView2.Text = tim.ToString("ddd");
+            weekNo = (int)tim.DayOfWeek;
+            showNo = weekNo;
+            if (showNo == 0)
+            {
+                showNo++;
+                first = false;
+                textView3.Text = "";
+                textView4.Text = "本日の予定";
+                textView5.Text = "";
+                textView6.Text = "本日は休日です";
+                textView7.Text = "";
+                textView8.Text = "";
+                textView9.Text = "";
+                textView10.Text = "";
+                textView11.Text = "";
+            }
+            ShowData_jumper();
+        }
+        void TimeReader()
+        {
+            if ((tim.Hour >= 0 && tim.Hour <= 8) || (tim.Hour == 9 && tim.Minute <= 19)) { classNo = 1; }
+            else if ((tim.Hour == 9 && tim.Minute >= 20) || tim.Hour == 10) { classNo = 2; }
+            else if ((tim.Hour == 11 || tim.Hour == 12) || (tim.Hour == 13 && tim.Minute <= 19)) { classNo = 3; }
+            else if ((tim.Hour == 13 && tim.Minute >= 20) || tim.Hour == 14) { classNo = 4; }
+            else if (tim.Hour == 15 || (tim.Hour == 16 && tim.Minute <= 39)) { classNo = 5; }
+            else if ((tim.Hour == 16 && tim.Minute >= 40) || tim.Hour >= 17) { ClassData_end(); }
+            if ((tim.Hour >= 0 && tim.Hour <= 15) || (tim.Hour == 16 && tim.Minute <= 39)) { ClassData_jumper(); }
+        }
+        void ShowData_jumper()
+        {
+            switch (showNo)
+            {
+                case 1:
+                    if (first == false) { textView12.Text = "月曜日の予定"; }
+                    ShowData_mon();
+                    break;
+                case 2:
+                    if (first == false) { textView12.Text = "火曜日の予定"; }
+                    ShowData_tue();
+                    break;
+                case 3:
+                    if (first == false) { textView12.Text = "水曜日の予定"; }
+                    ShowData_wed();
+                    break;
+                case 4:
+                    if (first == false) { textView12.Text = "木曜日の予定"; }
+                    ShowData_thr();
+                    break;
+                case 5:
+                    if (first == false) { textView12.Text = "金曜日の予定"; }
+                    ShowData_fri();
+                    break;
+                case 6:
+                    if(first == false) { textView12.Text = "土曜日の予定"; }
+                    ShowData_sat();
+                    break;
+            }
+            first = false;
+        }
+        void PopData_jumper()
+        {
+            switch (showNo)
+            {
+                case 1:
+                    PopData_mon();
+                    break;
+                case 2:
+                    PopData_tue();
+                    break;
+                case 3:
+                    PopData_wed();
+                    break;
+                case 4:
+                    PopData_thr();
+                    break;
+                case 5:
+                    PopData_fri();
+                    break;
+                case 6:
+                    PopData_sat();
+                    break;
+            }
+        }
+        void ClassData_jumper()
+        {
+            switch (weekNo)
+            {
+                case 1:
+                    ClassData_mon();
+                    break;
+                case 2:
+                    ClassData_tue();
+                    break;
+                case 3:
+                    ClassData_wed();
+                    break;
+                case 4:
+                    ClassData_thr();
+                    break;
+                case 5:
+                    ClassData_fri();
+                    break;
+                case 6:
+                    ClassData_sat();
+                    break;
+                default:
+                    break;
+            }
+        }
+        void ClassData_end()
+        {
+            textView3.Text = "";
+            textView4.Text = "本日の予定";
+            textView5.Text = "";
+            textView6.Text = "本日の予定はすべて終了しました";
+            textView7.Text = "";
+            textView8.Text = "";
+            textView9.Text = "";
+            textView10.Text = "";
+            textView11.Text = "";
+            Button2_click(null,null);
+        }
+    }
+}
+
